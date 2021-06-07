048143d0b7a6f3aee57898ba67e25d208d5ed9f1
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-07 09:45:25 +0200

    Make the state question more interesting

--- function3.hs
+++ function3.hs
@@ -1,6 +1,9 @@
 import Data.Yaml as Yaml
 
-data State
+data State = State {stateRatio :: Double}
+
+instance ToJSON State where
+  toJSON = error "Not implemented yet."
 
 instance FromJSON State where
   parseJSON = error "Not implemented yet."
